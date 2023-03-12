b2d7109a01e2a8dc135f8b1a5981758a4d096b70
FLab <FLab@DESKTOP-7KRHQRU>
2023-03-12 11:20:36 +0600

    Add project files.

---
+++ Oki/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Oki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oki.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly OkiDbContext db = new OkiDbContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+          var count =  db.Customers.Count();
+            ViewBag.Count = count;
+            ViewData["FirstCustomer"] = db.Customers.First().CompanyName;
+
+
+            return View(db.Customers.ToList());
+            //return View(db.Customers.OrderBy(c =>c.CustomerID).Skip(10).Take(10).ToList());
+        }
+    }
+}
